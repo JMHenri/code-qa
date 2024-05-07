4ffebfe5aca132267b3d6cdae1fc9b81b3ed2fbc
Jacob Henri <jakehenri@gmail.com>
2024-05-07 14:24:29 -0400

    change to degit for cleaner git download

--- scripts/download-repo.js
+++ scripts/download-repo.js
@@ -1,9 +1,6 @@
 require("dotenv").config();
-const simpleGit = require('simple-git');
-const fs = require("fs");
+const degit = require('degit');
 const path = require("path");
-
-const accessToken = process.env.GITHUB_ACCESS_TOKEN;
 
 // Repository details
 const owner = process.env.REPO_OWNER;
@@ -15,15 +12,18 @@ const localDir = path.join(__dirname, "../downloaded-repo");
 
 async function cloneRepository() {
   try {
-    // Initialize a new SimpleGit instance
-    const git = simpleGit();
+    // Construct the repository URL
+    const repoUrl = `${owner}/${repo}`;
 
-    // Construct the repository URL with authentication
-    const repoUrl = `https://github.com/stitionai/devika`;
-    // const repoUrl = `https://${accessToken}@github.com/${owner}/${repo}.git`;
+    // Create a degit emitter
+    const emitter = degit(repoUrl, {
+      cache: false,
+      force: true,
+      verbose: true,
+    });
 
     // Clone the repository
-    await git.clone(repoUrl, localDir, ['--branch', branch, '--single-branch']);
+    await emitter.clone(localDir);
 
     console.log("Repository cloned successfully!");
   } catch (error) {
