5392f47f6db83af4ef8e7069e589a76800b5ba6e
Jacob Henri <jakehenri@gmail.com>
2024-05-08 16:07:49 -0400

    core-planner cleanup

--- llm/prompts/core-planner.js
+++ llm/prompts/core-planner.js
@@ -1,20 +1,18 @@
 const corePlannerPrompt = ({ userQuestion, filePickerResponses, fileReaderResponses }) => `
-You are the core-planning module for a system of llms designed to help users ask questions about codebases. You will be given a user prompt,
-A list of files
-And a list of file summaries
+You are the core-planning module for a system of LLMs designed to help users ask questions about codebases. You will be given a user prompt, a list of files, and a list of file summaries. Your job is to:
 
-Your job is to:
 1. Call the filePicker if there are no files yet. It will find the files that are useful to answer the user prompt.
 2. Then, call the fileReader with the filePicker responses. It will read the contents of the files and figure out what is useful in each of them.
 3. If you have File Summaries, you should ALWAYS RETURN AN ANSWER by calling the summarize function!
 
-
 ---the user input is---
 ${userQuestion || 'empty'}
+
 ---FILES (if there are files here, CALL FILEREADER AND PASS THEM IN)---
-${filePickerResponses || 'empty'}
+${filePickerResponses ? filePickerResponses.join('\n') : 'empty'}
+
 ---FILE SUMMARIES (fill this out by sending FILES to fileReader!)---
-${fileReaderResponses || 'empty'}
+${fileReaderResponses ? fileReaderResponses.map(([filePath, response]) => `File: ${filePath}\nSummary: ${response}`).join('\n\n') : 'empty'}
 `;
 
 export default corePlannerPrompt;
